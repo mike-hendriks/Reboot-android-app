0071dd6ea279518c48fa2828c9e57c905b5e7371
Broek,Nina N. van den <ninavandenbroek29@gmail.com>
2019-01-10 14:26:56 +0100

    Knop werkt

--- app/src/main/java/com/example/jaron/accelerometer/LoadActivity.java
+++ app/src/main/java/com/example/jaron/accelerometer/LoadActivity.java
@@ -1,13 +1,33 @@
 package com.example.jaron.accelerometer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoadActivity extends AppCompatActivity {
+
+    private Button Cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        Cancel = (Button) findViewById(R.id.btncancel);
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnCancel();
+            }
+        });
     }
+
+    private void btnCancel() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
 }
