de9bb99a4100d8da77ebc81d225c91026e3b53aa
Bart <b.vdklundert@outlook.com>
2019-01-16 21:25:47 +0100

    Made progressbar + fixed width en height of points

--- app/src/main/java/com/example/jaron/accelerometer/LoadActivity.java
+++ app/src/main/java/com/example/jaron/accelerometer/LoadActivity.java
@@ -1,11 +1,14 @@
 package com.example.jaron.accelerometer;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
