71f06e69097e467dbdffb27c141023916ae370c9
stijnvdommelen <sipvandommelen@hotmail.com>
2019-01-17 09:30:28 +0100

    Points worden in database user gezet

--- app/src/main/java/com/example/jaron/accelerometer/ResultActivity.java
+++ app/src/main/java/com/example/jaron/accelerometer/ResultActivity.java
@@ -1,6 +1,7 @@
 package com.example.jaron.accelerometer;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Locale;
 
 public class ResultActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser currentFireBaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
     int reps = 0;
     long time;
     TextView result, timeLeft;
@@ -40,6 +50,8 @@ public class ResultActivity extends AppCompatActivity {
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         timeLeft.setText(timeLeftFormatted);
 
+        WritePointsToUser();
+
         ExitResult = (ImageButton) findViewById(R.id.ExitResult);
         ExitResult.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +65,27 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         finish();
         startActivity(intent);
+    }
+
+    private void WritePointsToUser()
+    {
+        DocumentReference docRef = db.collection("user").document(currentFireBaseUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        reps += document.getLong("points").intValue();
+                        db.collection("user").document(currentFireBaseUser.getUid()).update(
+                                "points", reps
+                        );
+
+                    } else {
+                    }
+                } else {
+                }
+            }
+        });
     }
 }
